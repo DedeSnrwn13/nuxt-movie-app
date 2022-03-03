c81fff59d074cfb43551653cc4086178d177360b
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-03-03 16:16:05 +0700

    Fetch Hook (Get movie data)

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,28 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  async fetch() {
+    await this.getMovies();
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        "https://anime-facts-rest-api.herokuapp.com/api/v1"
+      );
+      const result = await data;
+
+      result.data.data.forEach((movie) => {
+        this.movies.push(movie);
+      });
+      console.log(this.movies);
+    },
+  },
+};
 </script>
